46e16f0b4bc1222be9a82f52354d1b4ee921ccf4
Adam Dawidziuk <adamdawidziuk@onet.pl>
2024-04-26 16:30:38 +0200

    NEW: App structure

--- Uni_schedule_app/app/src/main/java/com/example/unischedule/MainActivity.kt
+++ Uni_schedule_app/app/src/main/java/com/example/unischedule/MainActivity.kt
@@ -5,11 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.unischedule.presentation.main_screen.MainScreen
+import com.example.unischedule.ui.theme.BackgroundColor
 import com.example.unischedule.ui.theme.UniScheduleTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,27 +18,11 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = Color.White
+                    color = BackgroundColor
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    UniScheduleTheme {
-        Greeting("Android")
     }
 }
---
+++ Uni_schedule_app/app/src/main/java/com/example/unischedule/presentation/main_screen/MainScreen.kt
@@ -0,0 +1,11 @@
+package com.example.unischedule.presentation.main_screen
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MainScreen(
+    
+) {
+    Text(text = "Hello world!")
+}
