e200bbb258e975c0aab268ac9c448fc7294edada
Adam Dawidziuk <adamdawidziuk@onet.pl>
2024-04-26 16:12:23 +0200

    NEW: Added dependencies

--- Uni_schedule_app/app/src/main/java/com/example/unischedule/MainActivity.kt
+++ Uni_schedule_app/app/src/main/java/com/example/unischedule/MainActivity.kt
@@ -4,11 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.unischedule.ui.theme.UniScheduleTheme
 
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Color.White
                 ) {
                     Greeting("Android")
                 }
